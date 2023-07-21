fda18e72ffb683cbbe805cc7f3fc3022b5afc69c
Amanda Young <botallytonkers@gmail.com>
2023-07-21 13:20:13 -0700

    starting

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,8 +4,22 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        Todo App
+        <h1>To-dos</h1>
+
+        <ul>
+          <li>Walk the dog</li>
+          <li>Take out trash</li>
+          <li>Workout</li>
+        </ul>
+
+        <form>
+          <input/>
+          <button>Add</button>
+        </form>
+
+        <button>Clear</button>
+
       </div>
-    )
+     )
   }
 }
